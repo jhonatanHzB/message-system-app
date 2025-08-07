2a33367df203c21fefe3ff7fa17dd52eb3ce581d
JhonatanHzB <jhonatanhzb@outlook.com>
2025-08-07 14:25:36 -0600

    Feat: Add Sanctum for API authentication and implement basic token-based login functionality

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'johndoe@message-app.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+Route::post('/login', function (Request $request) {
+
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (!Auth::attempt($credentials)) {
+        throw ValidationException::withMessages([
+            'email' => ['Los datos proporcionadas son incorrectos.'],
+        ]);
+    }
+
+    $user = Auth::user();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'Bearer',
+    ]);
+});
